ed8128c9a3c2c13cbf8a2f7ea0b14203f6a28839
blamphos <blamphos@hotmail.com>
2017-07-09 14:13:10 +0300

    First commit

---
+++ Elektrik/MainForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: RTM
+ * Date: 3.7.2017
+ * Time: 18:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Elektrik
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox comboBoxMonth;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comboBoxMonth = new System.Windows.Forms.ComboBox();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+			this.tableLayoutPanel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+			this.tableLayoutPanel3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(3, 3);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.IsValueShownAsLabel = true;
+			series1.LabelFormat = "####";
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(320, 216);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.923F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.077F));
+			this.tableLayoutPanel1.Controls.Add(this.chart2, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.chart1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(961, 445);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// chart2
+			// 
+			chartArea2.Name = "ChartArea1";
+			this.chart2.ChartAreas.Add(chartArea2);
+			this.chart2.Dock = System.Windows.Forms.DockStyle.Fill;
+			legend2.Name = "Legend1";
+			this.chart2.Legends.Add(legend2);
+			this.chart2.Location = new System.Drawing.Point(329, 3);
+			this.chart2.Name = "chart2";
+			series2.ChartArea = "ChartArea1";
+			series2.Legend = "Legend1";
+			series2.Name = "Series1";
+			this.chart2.Series.Add(series2);
+			this.chart2.Size = new System.Drawing.Size(629, 216);
+			this.chart2.TabIndex = 1;
+			this.chart2.Text = "chart2";
+			this.chart2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Chart2MouseClick);
+			this.chart2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Chart2MouseDown);
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 2;
+			this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 2);
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.75393F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.24607F));
+			this.tableLayoutPanel2.Controls.Add(this.chart3, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 1, 0);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 225);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(955, 217);
+			this.tableLayoutPanel2.TabIndex = 2;
+			// 
+			// chart3
+			// 
+			chartArea3.Name = "ChartArea1";
+			this.chart3.ChartAreas.Add(chartArea3);
+			this.chart3.Dock = System.Windows.Forms.DockStyle.Fill;
+			legend3.Name = "Legend1";
+			this.chart3.Legends.Add(legend3);
+			this.chart3.Location = new System.Drawing.Point(3, 3);
+			this.chart3.Name = "chart3";
+			series3.ChartArea = "ChartArea1";
+			series3.Legend = "Legend1";
+			series3.Name = "Series1";
+			this.chart3.Series.Add(series3);
+			this.chart3.Size = new System.Drawing.Size(727, 211);
+			this.chart3.TabIndex = 0;
+			this.chart3.Text = "chart3";
+			// 
+			// tableLayoutPanel3
+			// 
+			this.tableLayoutPanel3.ColumnCount = 2;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 43.05556F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 56.94444F));
+			this.tableLayoutPanel3.Controls.Add(this.label2, 0, 0);
+			this.tableLayoutPanel3.Controls.Add(this.comboBoxMonth, 1, 0);
+			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(736, 3);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.RowCount = 4;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(216, 211);
+			this.tableLayoutPanel3.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(3, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(87, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Kuukausi";
+			// 
+			// comboBoxMonth
+			// 
+			this.comboBoxMonth.FormattingEnabled = true;
+			this.comboBoxMonth.Location = new System.Drawing.Point(96, 3);
+			this.comboBoxMonth.Name = "comboBoxMonth";
+			this.comboBoxMonth.Size = new System.Drawing.Size(117, 21);
+			this.comboBoxMonth.TabIndex = 3;
+			this.comboBoxMonth.SelectedIndexChanged += new System.EventHandler(this.ComboBoxMonthSelectedIndexChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(961, 445);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "MainForm";
+			this.Text = "Elektrik";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+			this.tableLayoutPanel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+			this.tableLayoutPanel3.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Elektrik/MainForm.cs
@@ -0,0 +1,264 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: RTM
+ * Date: 3.7.2017
+ * Time: 18:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Elektrik
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{	
+		string _csvFileName;
+		RecordCollection _data;
+		readonly List<string> Months = new List<string> { "Tammi", "Helmi", "Maalis", "Huhti", "Touko", "Kesä", "Heinä", "Elo", "Syys", "Loka", "Marras", "Joulu" };
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+						
+			_csvFileName = Directory.GetFiles(Application.StartupPath, "*.csv").FirstOrDefault();
+			if (string.IsNullOrEmpty(_csvFileName))
+		    {
+				MessageBox.Show("CSV-file not found from startup path!");
+				return;
+		    }
+			
+			Text += " - " + Path.GetFileNameWithoutExtension(_csvFileName) + " (Riekonmarjatie 18 A 5)";
+			
+			_data = new RecordCollection();
+			ReadCsvFile();
+			InitGui();
+								
+			foreach (var year in _data.Years)
+			{
+				var yearLabel = year.ToString();
+				
+				// Total per year			
+				chart1.Series[0].Points.AddXY(yearLabel, _data.YearTotalKwh(year));
+
+				// Total per month	
+				var series = new Series(yearLabel);				
+				series.ChartType = SeriesChartType.Column;						
+				//series.IsValueShownAsLabel = true;
+				chart2.Series.Add(series);
+				
+				for (var i = 1; i <= 12; i++)
+				{
+					series.Points.AddXY(Months[i - 1], _data.MonthlyTotalKwh(year, i));
+				}
+			}	
+		}
+		
+		void InitGui()
+		{
+			chart1.Legends.Clear();
+			chart1.ChartAreas[0].AxisY.Title = "kWh";
+			chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+			chart1.Titles.Add(new Title("Vuosikulutus"));
+			chart1.Series[0].IsValueShownAsLabel = true;
+			
+			chart2.Series.Clear();
+			chart2.ChartAreas[0].AxisX.LabelStyle.Interval = 1;
+			//chart2.ChartAreas[0].AxisX.LabelStyle.Angle = 45;
+			chart2.ChartAreas[0].AxisX.LabelAutoFitStyle = LabelAutoFitStyles.LabelsAngleStep45;
+			chart2.ChartAreas[0].AxisY.Title = "kWh";
+			chart2.Titles.Add(new Title("Kuukausikulutus"));
+			
+			chart3.Series.Clear();
+			chart3.ChartAreas[0].AxisX.LabelStyle.Interval = 1;
+			chart3.ChartAreas[0].AxisX.LabelAutoFitStyle = LabelAutoFitStyles.LabelsAngleStep45;
+			chart3.ChartAreas[0].AxisY.Title = "kWh";
+			chart3.Titles.Add(new Title("Päiväkulutus"));
+			
+			// Comboboxes
+			comboBoxMonth.SelectedIndexChanged -= ComboBoxMonthSelectedIndexChanged;
+			comboBoxMonth.Items.AddRange(Months.ToArray());
+			comboBoxMonth.SelectedIndex = DateTime.Now.Month - 1;
+			comboBoxMonth.SelectedIndexChanged += ComboBoxMonthSelectedIndexChanged;
+
+			UpdateDayChart();
+		}
+		
+		void ReadCsvFile()
+		{
+			try 
+			{
+				using (var reader = new StreamReader(_csvFileName))
+				{
+					string line;
+					var header = false;
+					while ((line = reader.ReadLine()) != null)
+					{
+						if (!header)
+						{
+							header = true;
+							continue;
+						}
+						
+						var arr = line.Split(';');
+						var dt = DateTime.Parse(arr[0] + " " + arr[1]);
+						var kwh = Convert.ToDouble(arr[2]);
+						var temp = Convert.ToDouble(arr[3]);
+						
+						_data.Add(new Record(dt, kwh, temp));
+					}
+				}
+			} 
+			catch (Exception e)
+			{
+				MessageBox.Show("ReadCsvFile", e.Message);
+			}
+		}
+
+		void UpdateDayChart()
+		{		
+			chart3.Series.Clear();
+			var month = comboBoxMonth.SelectedIndex + 1;
+			
+			foreach (var year in _data.Years)
+			{
+				var yearLabel = year.ToString();
+				
+				var monthData = _data.GetMonthData(year, month);
+				var dayCount = monthData.Select(x => x.Timestamp.Day).Distinct().Count();
+
+				// Total per month	
+				var series = new Series(yearLabel);
+				series.ChartType = SeriesChartType.Column;
+				//series.BorderWidth = 12;
+				//series.IsValueShownAsLabel = true;
+				chart3.Series.Add(series);
+				
+				for (var i = 1; i <= dayCount; i++)
+				{
+					series.Points.AddXY(i.ToString(), _data.DailyTotalKwh(year, month, i));
+					//series.Points.AddXY(i.ToString(), _data.DailyAverageKwh(year, month, i));
+				}
+			}				
+		}		
+		
+		void ComboBoxMonthSelectedIndexChanged(object sender, EventArgs e)
+		{
+			UpdateDayChart();
+		}
+		
+		void Chart2MouseClick(object sender, MouseEventArgs e)
+		{
+
+		}
+		void Chart2MouseDown(object sender, MouseEventArgs e)
+		{
+		    var result = chart2.HitTest(e.X, e.Y);		        		  
+		    
+		    if (result.ChartElementType == ChartElementType.DataPoint)
+		    {
+		    	//var selectedColumn = chart2.Series[0].Points[result.PointIndex];
+			    var labelx = result.Series.Points[result.PointIndex].AxisLabel;
+			    Debug.WriteLine(labelx);		    
+		    }		
+		}
+	}
+	
+	public class Record
+	{
+		public DateTime Timestamp;
+		public double KwhTotal;
+		public double Temperature;
+		
+		public Record(DateTime dt, double kwh, double temp)
+		{
+			Timestamp = dt;
+			KwhTotal = kwh;
+			Temperature = temp;
+		}
+	}
+	
+	public sealed class RecordCollection
+	{
+		public List<Record> Items;
+		
+		public RecordCollection()
+		{
+			Items = new List<Record>();
+		}
+		
+		public void Add(Record record)
+		{
+			Items.Add(record);
+		}
+		
+		public List<int> Years
+		{
+			get
+			{
+				return Items.Select(x => x.Timestamp.Year).Distinct().ToList();	
+			}			
+		}
+		
+		public List<Record> GetYearData(int year)
+		{
+			return Items.Where(x => x.Timestamp.Year == year).ToList();
+		}
+		
+		public List<Record> GetMonthData(int year, int month)
+		{
+			var yearData = GetYearData(year);	
+			return yearData.Where(x => x.Timestamp.Month == month).ToList();
+		}
+		
+		public List<Record> GetDayData(int year, int month, int day)
+		{
+			var monthlyData = GetMonthData(year, month);
+			return monthlyData.Where(x => x.Timestamp.Day == day).ToList();
+		}
+		
+		public double YearTotalKwh(int year)
+		{
+			return GetYearData(year).Sum(x => x.KwhTotal);
+		}
+
+		public double MonthlyTotalKwh(int year, int month)
+		{						
+			return GetMonthData(year, month).Sum(x => x.KwhTotal);
+		}
+		
+		public double DailyTotalKwh(int year, int month, int day)
+		{							
+			return GetDayData(year, month, day).Sum(x => x.KwhTotal);
+		}
+		
+		public double DailyAverageKwh(int year, int month, int day)
+		{							
+			return GetDayData(year, month, day).Average(x => x.KwhTotal);
+		}
+		
+		public List<double> MonthlyTotalKwh(int year)
+		{
+			var yearData = GetYearData(year);
+			
+			var list = new List<double>();
+			for (var i = 1; i <= 12; i++)
+			{
+				list.Add(yearData.Where(x => x.Timestamp.Month == i).Sum(x => x.KwhTotal));
+			}
+			
+			return list;
+		}
+	}
+}
